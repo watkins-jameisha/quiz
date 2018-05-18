2cc2ce97a2e74b8b58bf376dbcd6c75ff9931cf8
watkins-jameisha <watkinsjameisha@gmail.com>
2018-05-18 11:20:29 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("container");
+var container = document.createElement("div")
+var x = document.createElement("INPUT");
+x.setAttribute("type", "button");
+var button = document.createElement("button");
+button.innerHTML = "SUBMIT";
+button.addEventListener ("click", function() {
+    alert("did something");
+  });
+  var total = 0;
+  for (var i = 0; i < someArray.length; i++) {
+      total += someArray[i] << 0;
+    }
